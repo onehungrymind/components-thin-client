95c858e64e828af2b96af8268f4d10ee4d8d7235
Lukas Ruebbelke <simpul@gmail.com>
2017-10-26 08:04:56 -0700

    update: step and cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as reducers from './shared/reducers';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/zip';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +17,15 @@ export class AppComponent {
     { path: '/home', icon: 'home', label: 'Home'},
     { path: '/clients', icon: 'face', label: 'Clients'},
     { path: '/projects', icon: 'work', label: 'Projects'}
+  ];
+
+  actions = [
+    { type: '[Client] Select', payload: { id: '1', firstName: 'John', lastName: 'Doe', company: 'Acme, Inc'}},
+    { type: '[Client] Select', payload: { id: '2', firstName: 'Jane', lastName: 'Smith', company: 'Super, Inc'}},
+    { type: '[Client] Select', payload: { id: '1', firstName: 'John', lastName: 'Doe', company: 'Acme, Inc'}},
+    { type: '[Client] Select', payload: { id: '2', firstName: 'Jane', lastName: 'Smith', company: 'Super, Inc'}},
+    { type: '[Client] Select', payload: { id: '1', firstName: 'John', lastName: 'Doe', company: 'Acme, Inc'}},
+    { type: '[Client] Select', payload: { id: '2', firstName: 'Jane', lastName: 'Smith', company: 'Super, Inc'}},
   ];
 
   index = 0;
@@ -26,18 +40,18 @@ export class AppComponent {
     this.store.dispatch({type: 'REDO'});
   }
 
-  test() {
-    const actions = [
-      { type: '[Client] Select', payload: { id: '1', firstName: 'John', lastName: 'Doe', company: 'Acme, Inc'}},
-      { type: '[Client] Select', payload: { id: '2', firstName: 'Jane', lastName: 'Smith', company: 'Super, Inc'}},
-      { type: '[Client] Select', payload: { id: '1', firstName: 'John', lastName: 'Doe', company: 'Acme, Inc'}},
-      { type: '[Client] Select', payload: { id: '2', firstName: 'Jane', lastName: 'Smith', company: 'Super, Inc'}},
-      { type: '[Client] Select', payload: { id: '1', firstName: 'John', lastName: 'Doe', company: 'Acme, Inc'}},
-      { type: '[Client] Select', payload: { id: '2', firstName: 'Jane', lastName: 'Smith', company: 'Super, Inc'}},
-    ];
+  step() {
+    this.store.dispatch(this.actions[this.index]);
 
-    this.store.dispatch(actions[this.index]);
+    this.index = this.index < this.actions.length - 1 ? this.index + 1 : 0;
+  }
 
-    this.index = this.index < actions.length - 1 ? this.index + 1 : 0;
+  cycle() {
+    const result = Observable
+      .from(this.actions)
+      .zip(Observable.interval(500), (a, b) => a)
+    ;
+
+    result.subscribe(action => this.store.dispatch(action));
   }
 }
